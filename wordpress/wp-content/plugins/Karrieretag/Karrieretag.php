03f7dbd4337cd5efcaf362893b21bcd2c06da9bb
Simon Schubert <simon.schubert@htl-wels.at>
2023-03-10 00:38:20 +0100

    some tests, can get forms, trying to get form input

--- wordpress/wp-content/plugins/Karrieretag/Karrieretag.php
+++ wordpress/wp-content/plugins/Karrieretag/Karrieretag.php
@@ -1,20 +1,86 @@
 <?php
     /*
-    * Plugin Name:       AppleSucks
-    * Description:       Handle the basics with this plugin.
-    * Version:           1.10.3
+    * Plugin Name: AppleSucks
+    * Description: Handle the basics with this plugin.
+    * Version: 1.10.3
     * Requires at least: 5.2
-    * Requires PHP:      7.2
+    * Requires PHP: 7.2
     */
     add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
-  
+
     function my_custom_dashboard_widgets() {
         global $wp_meta_boxes;
-    
+
         wp_add_dashboard_widget('custom_help_widget', 'AppleSucks', 'custom_dashboard_help');
     }
-        
+
     function custom_dashboard_help() {
-    echo '<p>Welcome to Custom Blog Theme! Need help? Contact the developer <a href="mailto:yourusername@gmail.com">here</a>. For WordPress Tutorials visit: <a href="https://www.wpbeginner.com" target="_blank">WPBeginner</a></p>';
+        //toString();
+        $array  = array('title' => 'contact', 'checked' => 'true')
+        for ()
+        //$_GET["contact"];
     }
+
+    function toString(){
+        echo "<form method='get'>";
+        for($i = 0; $i < count(find_forms_on_website()); $i++){
+            $form_name = get_form_name(find_forms_on_website()[$i]);
+            echo "<input type='checkbox' . $form_name . > " . $form_name . " <input type='button' value='putout'>" ."<br>";
+        }
+        echo "<input type='submit'></form>";
+    }
+
+    function find_forms_on_website() {
+        $args = array(
+            'post_type' => 'any',
+            'post_status' => 'publish',
+            'posts_per_page' => -1
+        );
+
+        $posts = get_posts($args);
+
+        $forms = array();
+
+        foreach ($posts as $post) {
+            preg_match_all('/<form.*<\/form>/siU', $post->post_content, $matches);
+            foreach ($matches[0] as $match) {
+                $forms[] = $match;
+            }
+        }
+
+        return $forms;
+    }
+
+    function get_form_name($form) {
+        $doc = new DOMDocument();
+        @$doc->loadHTML($form);
+        $form_name = $doc->getElementsByTagName('form')->item(0)->getAttribute('name');
+        return $form_name;
+    }
+
+    public function form($instance) {
+    $title = isset($instance['title']) ? $instance['title'] : '';
+    $checked = isset($instance['checked']) ? $instance['checked'] : false;
+    $submitted = isset($_POST[$this->get_field_name('submitted')]) ? true : false;
+    $data = isset($_POST[$this->get_field_name('data')]) ? $_POST[$this->get_field_name('data')] : '';
+
+    ?>
+    <p>
+        <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+    </p>
+    <p>
+        <label for="<?php echo $this->get_field_id('checked'); ?>"><?php _e('Checkbox:'); ?></label>
+        <input id="<?php echo $this->get_field_id('checked'); ?>" name="<?php echo $this->get_field_name('checked'); ?>" type="checkbox" value="1" <?php checked($checked, true); ?>>
+    </p>
+    <p>
+        <input type="hidden" name="<?php echo $this->get_field_name('submitted'); ?>" value="1" />
+        <input type="text" name="<?php echo $this->get_field_name('data'); ?>" value="<?php echo esc_attr($data); ?>" />
+        <input type="submit" value="<?php _e('Save'); ?>" class="button button-primary" />
+    </p>
+    <?php
+    if ($submitted) {
+        echo '<p>Submitted data: ' . $data . '</p>';
+    }
+}
 ?>
