b3fa3651afedbea84ca75871382756b63c9b4320
Simon Schubert <simon.schubert@htl-wels.at>
2023-03-11 18:02:08 +0100

    trying to submit form to database

--- wordpress/wp-content/plugins/Karrieretag/Karrieretag.php
+++ wordpress/wp-content/plugins/Karrieretag/Karrieretag.php
@@ -9,33 +9,89 @@
     add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
 
     function my_custom_dashboard_widgets() {
-        global $wp_meta_boxes;
-
         wp_add_dashboard_widget('custom_help_widget', 'AppleSucks', 'custom_dashboard_help');
     }
 
     function custom_dashboard_help() {
         toString();
+        write_in_database();
+    }
+
+    function define_forms(){
+        $args = array(
+            'post_type' => 'any',
+            'post_status' => 'publish',
+            'posts_per_page' => -1
+        );
+
+        $posts = get_posts($args);
+
+        $submissions = array();
+
+        foreach ($posts as $post) {
+            // Check if the post ID is in the specified form IDs
+            if (in_array($post->ID, set_forms_for_Database())) {
+                // Use regular expression to extract form submissions
+                preg_match_all('/<form.*<\/form>/siU', $post->post_content, $matches);
+                foreach ($matches[0] as $match) {
+                    // Process and store the form submissions as needed
+                    $submissions[] = process_form_submissions($match);
+                }
+            }
+        }
+
+        return $submissions;
+    }
+
+    function write_in_database(){
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'DataFromSubmissions';
+
+        foreach (define_forms() as $data) {
+            $wpdb->insert($table_name, $data);
+        }
+
+        /*$dsn = 'mysql:host=localhost;dbname=wordpress;charset=utf8mb4';
+        $username = 'wordpress';
+        $password = 'Pa$$word1234';
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        );
+        $pdo = new PDO($dsn, $username, $password, $options);
+
+// Prepare the SQL statement
+        $stmt = $pdo->prepare('INSERT INTO mytable (column1, column2, column3) VALUES (:value1, :value2, :value3)');
+
+// Bind the parameter values
+        $value1 = 'some value';
+        $value2 = 123;
+        $value3 = date('Y-m-d H:i:s');
+        $stmt->bindParam(':value1', $value1);
+        $stmt->bindParam(':value2', $value2);
+        $stmt->bindParam(':value3', $value3);
+
+// Execute the SQL statement
+        $stmt->execute();
+        */
+    }
+
+    function set_forms_for_Database(){
+        $forms = array();
 
         if(!empty($_POST)){
             foreach($_POST as $key => $value){
-                // Preprocess $key which holds name of input field
-                // You can apply your logic to process value for an input $key here
-                // From your example it looks like name is a number so special case can check within a condition for $key as number
-
-                echo $key . "=" . $value;
+                if($value == "on") {
+                    $forms[] = $key;
+                }
             }
         }
-    }
 
-    function arrayToString($string){
-        foreach($string as $key => $value){
-            echo $value;
-        }
+        return $forms;
     }
 
     function toString(){
-        if (isset($_POST['submit'])) {
+        /*if (isset($_POST['submit'])) {
             $data = array();
             if(!empty($_POST)){
                 foreach($_POST as $key => $value){
@@ -43,28 +99,20 @@
                 }
             }
 
-            foreach($_POST as $key => $value) {
-                if (isset($value)) {
-                    $data[$key] = true;
-                } else {
-                    $data[$key] = false;
-                }
-            }
-
             update_option('my_dashboard_widget_data', $data);
         }
 
-        $data = get_option('my_dashboard_widget_data');
-
-        arrayToString($data);
+        $data = get_option('my_dashboard_widget_data');*/
 
         echo "<form method='post'>";
         for($i = 0; $i < count(find_forms_on_website()); $i++){
             $form_name = get_form_name(find_forms_on_website()[$i]);
-            echo "<input type='checkbox' name='.$form_name.' value='.$data[$form_name].'> " . $form_name . " <input type='button' value='putout'>" ."<br>";
+            echo "<input type='checkbox' name='.$form_name.'> " . $form_name . " <input type='button' value='putout'>" ."<br>";
         }
         echo "<input type='submit'></form>";
     }
+
+
 
     function find_forms_on_website() {
         $args = array(
@@ -87,6 +135,10 @@
         return $forms;
     }
 
+    function get_form(){
+
+    }
+
     function get_form_name($form)
     {
         $doc = new DOMDocument();
@@ -94,30 +146,4 @@
         $form_name = $doc->getElementsByTagName('form')->item(0)->getAttribute('name');
         return $form_name;
     }
-
-    /*public function form($instance) {
-    $title = isset($instance['title']) ? $instance['title'] : '';
-    $checked = isset($instance['checked']) ? $instance['checked'] : false;
-    $submitted = isset($_POST[$this->get_field_name('submitted')]) ? true : false;
-    $data = isset($_POST[$this->get_field_name('data')]) ? $_POST[$this->get_field_name('data')] : '';
-
-    ?>
-    <p>
-        <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
-        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
-    </p>
-    <p>
-        <label for="<?php echo $this->get_field_id('checked'); ?>"><?php _e('Checkbox:'); ?></label>
-        <input id="<?php echo $this->get_field_id('checked'); ?>" name="<?php echo $this->get_field_name('checked'); ?>" type="checkbox" value="1" <?php checked($checked, true); ?>>
-    </p>
-    <p>
-        <input type="hidden" name="<?php echo $this->get_field_name('submitted'); ?>" value="1" />
-        <input type="text" name="<?php echo $this->get_field_name('data'); ?>" value="<?php echo esc_attr($data); ?>" />
-        <input type="submit" value="<?php _e('Save'); ?>" class="button button-primary" />
-    </p>
-    <?php
-    if ($submitted) {
-        echo '<p>Submitted data: ' . $data . '</p>';
-    }
-}*/
 ?>
