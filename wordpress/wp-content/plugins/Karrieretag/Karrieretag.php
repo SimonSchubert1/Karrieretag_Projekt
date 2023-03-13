3a90adfec9cc9529cc26e4530ec4d1e766af15bd
Simon Schubert <simon.schubert@htl-wels.at>
2023-03-13 23:55:48 +0100

    added dynamic adding of databases

--- wordpress/wp-content/plugins/Karrieretag/Karrieretag.php
+++ wordpress/wp-content/plugins/Karrieretag/Karrieretag.php
@@ -48,7 +48,7 @@ class KarrieretagPlugin{
     function make_menu_page()
     {
         echo "<h1>AppleSucks</h1>";
-        echo "<form method='post' id='Forms-to-check'>";
+        echo "<form action='#' method='post' id='Forms-to-check'>";
         for ($i = 0; $i < count($this->find_forms_on_website()); $i++) {
             $form_name = $this->get_form_name($this->find_forms_on_website()[$i]);
             if (isset($_POST["_" . $form_name . "_"])) {
@@ -59,9 +59,71 @@ class KarrieretagPlugin{
 
             $this->form_names[] = $form_name;
         }
-        echo "<input type='submit'></form>";
+        echo "<input type='submit' name='submitbtn'></form>";
 
         echo "<a href='../wp-content/plugins/Karrieretag/exportData.php'>Export</a>";
+
+        /*foreach ($this->find_forms_on_website_full_code() as $key => $value){
+            $tag_str = "<".$value.">";
+            echo htmlspecialchars($tag_str);
+        }*/
+
+        if(isset($_POST['submitbtn'])) {
+            $formnames = array();
+            foreach ($_POST as $key => $value) {
+                if(strcmp($key, 'submitbtn')) {
+                    $formnames[] = $key;
+                }
+            }
+
+            //data of database
+            $dbHost = "localhost";
+            $dbUsername = "wordpress";
+            $dbPassword = "Pa$\$word1234";
+            $dbName = "test";
+
+            $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+            foreach ($formnames as $key) {
+                $query = "CREATE TABLE IF NOT EXISTS `" . $key . "` (";
+                foreach($this->find_forms_on_website_full_code() as $key3 => $value2) {
+                    $comp = $this->get_form_name($value2);
+                    if(!strcmp($key, $comp)) {
+                        foreach ($this->get_input_tag_names($value2) as $key2 => $value) {
+                            $splitstring = explode(";", $value);
+                            $query .= "`" . $splitstring[0] . "`" . " " . $this->suggest_mysql_datatype($splitstring[1]) . ", ";
+                        }
+                    }
+                }
+
+                $query .= "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+
+                if (mysqli_query($db, $query)) {
+                    echo "<br> <b>Table created successfully</b>";
+                } else {
+                    echo "Error creating table: " . mysqli_error($db);
+                }
+            }
+        }
+    }
+
+    function suggest_mysql_datatype($input) {
+        if ($input == "number") {
+            return "INT(10)";
+        } elseif ($input == "text") {
+            return "VARCHAR(255)";
+        } elseif ($input == "checkbox") {
+            return "BOOLEAN";
+        } else {
+            return "VARCHAR(255)";
+        }
+    }
+
+    function get_form_name_from_code($html) {
+        $dom = new DOMDocument();
+        $dom->loadHTML($html);
+        $form = $dom->getElementsByTagName('form')->item(0);
+        return $form->getAttribute('name');
     }
 
     function find_forms_on_website() {
@@ -83,6 +145,49 @@ class KarrieretagPlugin{
         }
 
         return $forms;
+    }
+
+    function find_forms_on_website_full_code() {
+        $args = array(
+            'post_type' => 'any',
+            'post_status' => 'publish',
+            'posts_per_page' => -1
+        );
+
+        $posts = get_posts($args);
+
+        $forms = array();
+
+        foreach ($posts as $post) {
+            preg_match_all('/<form.*<\/form>/siU', $post->post_content, $matches);
+            foreach ($matches[0] as $match) {
+                $forms[] = $match;
+            }
+        }
+
+        return $forms;
+    }
+
+    function get_input_tag_names($form_html) {
+        $input_tag_names = array();
+
+        // Load the HTML form using the DOMDocument class
+        $dom = new DOMDocument();
+        @$dom->loadHTML($form_html);
+
+        // Get all input elements in the form
+        $input_tags = $dom->getElementsByTagName('input');
+
+        // Loop through each input element and extract the name attribute
+        foreach ($input_tags as $input) {
+            $name = $input->getAttribute('name');
+            $type = $input->getAttribute('type');
+            if (!empty($name)) {
+                $input_tag_names[] = $name . ";" . $type;
+            }
+        }
+
+        return $input_tag_names;
     }
 
     function get_form_name($form)
